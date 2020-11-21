70a6af01a63b3ea14201dce5d27705e56c384923
AndreyAD <arakelyants.andrey@yandex.ru>
2020-11-21 20:23:19 +0300

    Process the case of broken connection

--- registration.py
+++ registration.py
@@ -2,13 +2,13 @@ import asyncio
 import json
 from logger import logger
 import re
+import socket
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 
 from anyio import create_task_group
 
 from gui import TkAppClosed, update_tk
-from open_connection import open_connection
 
 
 def process_button_click(
@@ -17,6 +17,7 @@ def process_button_click(
         username_entry,
         request_info_queue
 ):
+    logger.debug('begin to process button click')
     host = host_entry.get()
     port = port_entry.get()
     username = username_entry.get()
@@ -27,28 +28,34 @@ def process_button_click(
 async def register(request_info_queue, new_user_hash_queue):
     while True:
         host, port, user_name = await request_info_queue.get()
-        async with open_connection(host, port) as (reader, writer):
-            server_response = await reader.readline()
-            logger.debug(repr(server_response.decode()))
+        logger.debug('Begin to register')
+        try:
+            reader, writer = await asyncio.open_connection(host, port)
+        except socket.gaierror:
+            new_user_hash_queue.put_nowait('Нет соединения с сервером')
+            continue
 
-            writer.write(b'\n')
-            await writer.drain()
-            server_response = await reader.readline()
-            logger.debug(repr(server_response.decode()))
+        server_response = await reader.readline()
+        logger.debug(repr(server_response.decode()))
 
-            filtered_nick = re.sub(r'(\\+n|\n|\\+)', '', user_name)
-            message_to_send = f'{filtered_nick}\n'
-            logger.debug(repr(message_to_send))
-            writer.write(message_to_send.encode())
-            await writer.drain()
+        writer.write(b'\n')
+        await writer.drain()
+        server_response = await reader.readline()
+        logger.debug(repr(server_response.decode()))
 
-            server_response = await reader.readline()
-            logger.debug(repr(server_response.decode()))
-            user_features = json.loads(server_response)
-            try:
-                user_token = user_features['account_hash']
-            except (AttributeError, KeyError):
-                user_token = None
+        filtered_nick = re.sub(r'(\\+n|\n|\\+)', '', user_name)
+        message_to_send = f'{filtered_nick}\n'
+        logger.debug(repr(message_to_send))
+        writer.write(message_to_send.encode())
+        await writer.drain()
+
+        server_response = await reader.readline()
+        logger.debug(repr(server_response.decode()))
+        user_features = json.loads(server_response)
+        try:
+            user_token = user_features['account_hash']
+        except (AttributeError, KeyError):
+            user_token = None
 
         new_user_hash_queue.put_nowait(user_token)
 
@@ -58,8 +65,8 @@ async def update_hash_label(user_hash_label, new_user_hash_queue):
         user_hash = await new_user_hash_queue.get()
         if user_hash is None:
             # process the error
-            pass
-        user_hash_label['text'] = user_hash
+            user_hash = 'ERROR'
+        user_hash_label['text'] = f'Хэш нового пользователя: {user_hash}'
 
 
 async def draw(request_info_queue, new_user_hash_queue):
@@ -68,21 +75,18 @@ async def draw(request_info_queue, new_user_hash_queue):
     root_frame = tk.Frame()
     root_frame.pack(fill="both", expand=True)
 
-    host_entry = tk.Entry(width=20)
-    port_entry = tk.Entry(width=20)
-    user_name_entry = tk.Entry(width=20)
+    host_entry = tk.Entry(width=50)
+    port_entry = tk.Entry(width=50)
+    user_name_entry = tk.Entry(width=50)
     sign_up_button = tk.Button(text='Регистрация')
-    user_hash_label = tk.Label(width=20, bg='black', fg='white')
+    user_hash_label = tk.Label(width=50, bg='black', fg='white')
 
-    sign_up_button.bind(
-        'Sign Up',
-        lambda event: process_button_click(
+    sign_up_button['command'] = lambda: process_button_click(
             host_entry,
             port_entry,
             user_name_entry,
             request_info_queue
         )
-    )
 
     host_entry.pack()
     port_entry.pack()
@@ -95,6 +99,11 @@ async def draw(request_info_queue, new_user_hash_queue):
         await task_group.spawn(
             register,
             request_info_queue,
+            new_user_hash_queue
+        )
+        await task_group.spawn(
+            update_hash_label,
+            user_hash_label,
             new_user_hash_queue
         )
 
