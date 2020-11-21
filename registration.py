280505ae2aa347d7899054a6d2987a02d379a7c8
AndreyAD <arakelyants.andrey@yandex.ru>
2020-11-22 01:43:58 +0300

    Add labels to the entries

--- registration.py
+++ registration.py
@@ -20,7 +20,6 @@ def process_button_click(
         username_entry,
         request_info_queue
 ):
-    logger.debug('begin to process button click')
     host = host_entry.get()
     port = port_entry.get()
     username = username_entry.get()
@@ -32,14 +31,14 @@ async def register(request_info_queue, new_user_hash_queue):
     while True:
         host, port, user_name = await request_info_queue.get()
         server_address = f'{host}:{port}'
-        log_msg = f'Установка соединения с сервером {server_address}'
+        log_msg = f'Установка соединения с сервером "{server_address}"'
         logger.debug(log_msg)
         try:
             reader, writer = await asyncio.open_connection(host, port)
         except socket.gaierror:
-            log_msg = f'ОШИБКА. Нет соединения с сервером {server_address}'
+            log_msg = f'ОШИБКА. Нет соединения с сервером "{server_address}"'
             logger.error(log_msg)
-            new_user_hash_queue.put_nowait('ОШИБКА. Нет соединения с сервером')
+            new_user_hash_queue.put_nowait(log_msg)
             continue
 
         server_response = await reader.readline()
@@ -70,7 +69,7 @@ async def register(request_info_queue, new_user_hash_queue):
 async def update_hash_label(user_hash_label, new_user_hash_queue):
     while True:
         user_hash = await new_user_hash_queue.get()
-        user_hash_label['text'] = f'Хэш нового пользователя: {user_hash}'
+        user_hash_label['text'] = user_hash
 
 
 async def draw(request_info_queue, new_user_hash_queue, log_queue):
@@ -80,11 +79,15 @@ async def draw(request_info_queue, new_user_hash_queue, log_queue):
     upper_frame = tk.Frame(root_frame)
     lower_frame = tk.Frame(root_frame)
 
+    host_label = tk.Label(upper_frame, text='Адрес сервера')
     host_entry = tk.Entry(upper_frame, width=50)
+    port_label = tk.Label(upper_frame, text='Номер порта для регистрации')
     port_entry = tk.Entry(upper_frame, width=50)
+    user_name_label = tk.Label(upper_frame, text='Имя нового пользователя')
     user_name_entry = tk.Entry(upper_frame, width=50)
     sign_up_button = tk.Button(upper_frame, text='Регистрация')
-    user_hash_label = tk.Label(upper_frame, width=50, bg='black', fg='white')
+    hash_label = tk.Label(upper_frame, text='Хэш созданного пользователя')
+    user_hash_field = tk.Label(upper_frame, width=50, bg='white', fg='red')
     sign_up_button['command'] = lambda: process_button_click(
         host_entry,
         port_entry,
@@ -98,11 +101,15 @@ async def draw(request_info_queue, new_user_hash_queue, log_queue):
     root_frame.pack(fill="both", expand=True)
     upper_frame.pack()
     lower_frame.pack()
+    host_label.pack()
     host_entry.pack()
+    port_label.pack()
     port_entry.pack()
+    user_name_label.pack()
     user_name_entry.pack()
     sign_up_button.pack()
-    user_hash_label.pack()
+    hash_label.pack()
+    user_hash_field.pack()
 
     async with create_task_group() as task_group:
         await task_group.spawn(update_tk, root_frame)
@@ -113,7 +120,7 @@ async def draw(request_info_queue, new_user_hash_queue, log_queue):
         )
         await task_group.spawn(
             update_hash_label,
-            user_hash_label,
+            user_hash_field,
             new_user_hash_queue
         )
         await task_group.spawn(update_scroll_panel, log_panel, log_queue)
@@ -133,7 +140,7 @@ def main():
     logger.setLevel(logging.DEBUG)
     log_queue = asyncio.Queue()
     tkinter_log_handler = TkinterLogHandler(log_queue)
-    formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+    formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(message)s')
     tkinter_log_handler.setFormatter(formatter)
     logger.addHandler(tkinter_log_handler)
 
